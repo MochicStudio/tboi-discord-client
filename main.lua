1b85155e08103758d9982f85871230546a285ee7
Andres Ruiz <andrsruiz@gmail.com>
2021-03-30 22:06:57 -0600

    Update mod

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+--StartDebug()
 require("constants")
 require("config")
 local Socket = require("socket")
@@ -17,7 +18,7 @@ local userId = rng:Next()
 tcp:send(PACKET_SEND_CODES["AUTH"] .. userId)
 Isaac.ConsoleOutput("DiscordTBOI: Your token is " .. userId)
 
-DiscordTBOI:AddCallback(ModCallbacks.MC_POST_UPDATE, function (_mod)
+tboiDiscord:AddCallback(ModCallbacks.MC_POST_UPDATE, function (_mod)
 	local packet, err = tcp:receive(1)
 	if packet ~= nil then
 		local rest = tcp:receive()
@@ -25,4 +26,3 @@ DiscordTBOI:AddCallback(ModCallbacks.MC_POST_UPDATE, function (_mod)
 			PACKET_RECEIVE_CODES[packet](rest)
 		end
 end)
-
